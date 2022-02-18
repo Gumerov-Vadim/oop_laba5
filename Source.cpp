4a378196a9d0630267278c41298d693a47a79dad
Gumerov Vadim <vadim.ugatu@mail.ru>
2022-02-18 19:54:18 +0500

    add dynamic_cast examples

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <string>
 #include <conio.h>
+#include <ctime>
+int randomizer(int min, int max) {
+	time_t sec;
+	sec = time(NULL);
+	int r = (rand() * sec) % (max - min) + min;
+	return r;
+}
 namespace virtual_destructor {
 	class IBase {
 	protected:
@@ -16,21 +23,21 @@ namespace virtual_destructor {
 	public:
 		Base() { num1 = 0; num2 = 0; res; cname = 'b'; what_was_created(); }
 		Base(int num1, int num2) { this->num1 = num1; this->num2 = num2; cname = 'b'; what_was_created();}
-		virtual ~Base() { printf("~вызван деструктор ~Base()\n"); }
-		int take_away() { printf("вызван метод базового класса take_away()\n"); return res = num1 - num2; }
-		int result() { printf("вызван метод базового класса result()\n"); return res; }
+		virtual ~Base() { printf("~РІС‹Р·РІР°РЅ РґРµСЃС‚СЂСѓРєС‚РѕСЂ ~Base()\n"); }
+		int take_away() { printf("РІС‹Р·РІР°РЅ РјРµС‚РѕРґ Р±Р°Р·РѕРІРѕРіРѕ РєР»Р°СЃСЃР° take_away()\n"); return res = num1 - num2; }
+		int result() { printf("РІС‹Р·РІР°РЅ РјРµС‚РѕРґ Р±Р°Р·РѕРІРѕРіРѕ РєР»Р°СЃСЃР° result()\n"); return res; }
 		virtual void what_was_created() {
-				printf("был создан объект класса Base(%d,%d)\n", num1, num2);		
+				printf("Р±С‹Р» СЃРѕР·РґР°РЅ РѕР±СЉРµРєС‚ РєР»Р°СЃСЃР° Base(%d,%d)\n", num1, num2);		
 		}
 	};
 	class Desc : public Base{
 	public:
-		int take_away() { printf("вызван переопределённый метод класса наследника take_away()\n"); return res = num2 - num1; }
+		int take_away() { printf("РІС‹Р·РІР°РЅ РїРµСЂРµРѕРїСЂРµРґРµР»С‘РЅРЅС‹Р№ РјРµС‚РѕРґ РєР»Р°СЃСЃР° РЅР°СЃР»РµРґРЅРёРєР° take_away()\n"); return res = num2 - num1; }
 		Desc() :Base() {cname = 'c'; what_was_created();}
 		Desc(int num1, int num2) : Base(num1, num2) {cname = 'c'; what_was_created();}
-		void what_was_created() {printf("был создан объект класса Base(%d,%d)\n", num1, num2);
+		void what_was_created() {printf("Р±С‹Р» СЃРѕР·РґР°РЅ РѕР±СЉРµРєС‚ РєР»Р°СЃСЃР° Base(%d,%d)\n", num1, num2);
 		}
-		~Desc() { printf("~вызван деструктор ~Desc()\n"); }
+		~Desc() { printf("~РІС‹Р·РІР°РЅ РґРµСЃС‚СЂСѓРєС‚РѕСЂ ~Desc()\n"); }
 	};
 }
 namespace overriden_methods {
@@ -46,19 +53,19 @@ namespace overriden_methods {
 		char cname;
 	public:
 		Base() { cname = 'b'; what_was_created();}
-		void what_was_created() {printf("Создан объект Base()\n");}
-		virtual ~Base() { printf("~вызван деструктор ~Base()\n"); }
-		void v_first_meth() { printf("Вызван 1-ый метод класса base, "); v_second_meth(); }
-		void nv_first_meth() { printf("Вызван 1-ый метод класса base, "); nv_second_meth(); }
-		virtual void v_second_meth(){ printf("Вызван 2-ой метод класса base\n"); }
-		void nv_second_meth() { printf("Вызван 2-ой метод класса base\n"); }
+		void what_was_created() {printf("РЎРѕР·РґР°РЅ РѕР±СЉРµРєС‚ Base()\n");}
+		virtual ~Base() { printf("~РІС‹Р·РІР°РЅ РґРµСЃС‚СЂСѓРєС‚РѕСЂ ~Base()\n"); }
+		void v_first_meth() { printf("Р’С‹Р·РІР°РЅ 1-С‹Р№ РјРµС‚РѕРґ РєР»Р°СЃСЃР° base, "); v_second_meth(); }
+		void nv_first_meth() { printf("Р’С‹Р·РІР°РЅ 1-С‹Р№ РјРµС‚РѕРґ РєР»Р°СЃСЃР° base, "); nv_second_meth(); }
+		virtual void v_second_meth(){ printf("Р’С‹Р·РІР°РЅ 2-РѕР№ РјРµС‚РѕРґ РєР»Р°СЃСЃР° base\n"); }
+		void nv_second_meth() { printf("Р’С‹Р·РІР°РЅ 2-РѕР№ РјРµС‚РѕРґ РєР»Р°СЃСЃР° base\n"); }
 	};
 	class Desc : public Base {
 	public:
 		Desc() { cname = 'c'; what_was_created(); }
-		void what_was_created() {printf("Создан объект Desc()\n");}
-		void second_meth() { printf("Вызван 2-ой метод класса Desc\n"); }
-		~Desc() { printf("~вызван деструктор ~Desc()\n"); }
+		void what_was_created() {printf("РЎРѕР·РґР°РЅ РѕР±СЉРµРєС‚ Desc()\n");}
+		void second_meth() { printf("Р’С‹Р·РІР°РЅ 2-РѕР№ РјРµС‚РѕРґ РєР»Р°СЃСЃР° Desc\n"); }
+		~Desc() { printf("~РІС‹Р·РІР°РЅ РґРµСЃС‚СЂСѓРєС‚РѕСЂ ~Desc()\n"); }
 	};
 }
 namespace virtual_methods{
@@ -74,18 +81,18 @@ namespace virtual_methods{
 		char cname;
 	public:
 		Base() { cname = 'b'; what_was_created(); }
-		void what_was_created() { printf("Создан объект Base()\n"); }
-		virtual ~Base() { printf("~вызван деструктор ~Base()\n"); }
-		virtual void v_meth() { printf("Вызван виртуальный метод класса base\n");}
-		void nv_meth() { printf("Вызван невиртуальный метод класса base\n");}
+		void what_was_created() { printf("РЎРѕР·РґР°РЅ РѕР±СЉРµРєС‚ Base()\n"); }
+		virtual ~Base() { printf("~РІС‹Р·РІР°РЅ РґРµСЃС‚СЂСѓРєС‚РѕСЂ ~Base()\n"); }
+		virtual void v_meth() { printf("Р’С‹Р·РІР°РЅ РІРёСЂС‚СѓР°Р»СЊРЅС‹Р№ РјРµС‚РѕРґ РєР»Р°СЃСЃР° base\n");}
+		void nv_meth() { printf("Р’С‹Р·РІР°РЅ РЅРµРІРёСЂС‚СѓР°Р»СЊРЅС‹Р№ РјРµС‚РѕРґ РєР»Р°СЃСЃР° base\n");}
 	};
 	class Desc : public Base {
 	public:
 		Desc() { cname = 'c'; what_was_created(); }
-		void what_was_created() { printf("Создан объект Desc()\n"); }
-		void v_meth() { printf("Вызван виртуальный метод класса Desc\n"); }
-		void nv_meth() { printf("Вызван невиртуальный метод класса Desc\n"); }
-		~Desc() { printf("~вызван деструктор ~Desc()\n"); }
+		void what_was_created() { printf("РЎРѕР·РґР°РЅ РѕР±СЉРµРєС‚ Desc()\n"); }
+		void v_meth() { printf("Р’С‹Р·РІР°РЅ РІРёСЂС‚СѓР°Р»СЊРЅС‹Р№ РјРµС‚РѕРґ РєР»Р°СЃСЃР° Desc\n"); }
+		void nv_meth() { printf("Р’С‹Р·РІР°РЅ РЅРµРІРёСЂС‚СѓР°Р»СЊРЅС‹Р№ РјРµС‚РѕРґ РєР»Р°СЃСЃР° Desc\n"); }
+		~Desc() { printf("~РІС‹Р·РІР°РЅ РґРµСЃС‚СЂСѓРєС‚РѕСЂ ~Desc()\n"); }
 	};
 }
 namespace check_classes {
@@ -100,8 +107,8 @@ namespace check_classes {
 	};
 	class first_c :public Ic {
 	protected:
-		void was_deleted(){ std::cout << "объект класса " << name << " удален\n"; }
-		void what_was_created() { printf("Был создан объект класса "); std::cout << name<<"\n"; }
+		void was_deleted(){ std::cout << "РѕР±СЉРµРєС‚ РєР»Р°СЃСЃР° " << name << " СѓРґР°Р»РµРЅ\n"; }
+		void what_was_created() { printf("Р‘С‹Р» СЃРѕР·РґР°РЅ РѕР±СЉРµРєС‚ РєР»Р°СЃСЃР° "); std::cout << name<<"\n"; }
 	public:
 		std::string classname() {
 			return "first";
@@ -129,14 +136,80 @@ namespace check_classes {
 		~third_c() { was_deleted(); }
 	};
 }
-void next() {
-	switch (_getch())
-	{
-	default:
-		system("cls");
-		printf("\n");
-		break;
+namespace dynamic_cast_examples {
+	class Object {
+	private:
+		virtual void do_nothing() {
+			//nothing...
+		}
+		int id;
+	public:
+		void set_id(int id) { this->id = id; }
+		int get_id() { return this->id; }
+		Object() { id = 0; }
+	};
+	class Point :public Object{
+	private:
+		int x, y;
+	public:
+		friend bool operator==(Point p1, Point p2) {
+			bool eq; (p1.get_x() == p2.get_x() && p1.get_y() == p2.get_y()) ? eq = true : eq = false;
+			return eq;
+		}
+		friend bool operator!=(Point p1, Point p2) {
+			return !(p1 == p2);
+		}
+		int get_x() { return x; }
+		int get_y() { return y; }
+		void set(int x, int y) { this->x = x; this->y = y; }
+		Point() { x = 0; y = 0; }
+		Point(int x, int y) { this->x = x; this->y = y; }
+		Point(Point* p) { this->x = p->get_x(); this->y = p->get_y(); }
+	};
+	class Circle : public Point {
+	private:
+		int r;
+	public:
+		int get_radius() { return r; }
+		void set_radius(int r) { if (r > 0) { this->r = r; } }
+		Circle() : Point() { r = 1; }
+		Circle(int x, int y, int r) :Point(x, y) { r > 0 ? this->r = r : this->r = 1; }
+	};
+	class Triangle : public Object {
+	private:
+		Point p1, p2, p3;
+	public:
+		Triangle() { p1.set(0, 0); p2.set(1, 0);p1.set(0, 1);}
+		Triangle(Point p1, Point p2, Point p3) { if (p1 != p2 && p2 != p3) { this->p1 = p1; this->p2 = p2; this->p3 = p3; } else { p1.set(0, 0); p2.set(1, 0); p1.set(0, 1); } }
+	};
+	Object* create_rand_obj() {
+		int r = randomizer(0, 3);
+		Point p1, p2, p3;
+		switch (r)
+		{
+		case 0:
+			return new Point(randomizer(-10, 10), randomizer(-10, 10));
+			break;
+		case 1:
+			return new Circle(randomizer(-10, 10), randomizer(-10, 10), randomizer(3, 15));
+			break;
+		case 2:
+			p1.set(randomizer(-10, 10), randomizer(-10, 10));
+			p2.set(randomizer(-10, 10), randomizer(-10, 10));
+			p3.set(randomizer(-10, 10), randomizer(-10, 10));
+			return new Triangle(p1, p2, p3);
+			break;
+		default:
+			printf("error:imposible case\n");
+			return NULL;
+			break;
+		}
 	}
+}
+void next() {
+	_getch();
+	system("cls");
+	printf("\n");
 }
 int main() {
 	setlocale(LC_ALL, "Russian"); 
@@ -145,21 +218,21 @@ int main() {
 		using namespace virtual_destructor;
 		Base* desc = new Desc(5,3);
 		desc->take_away();
-		printf("рельзутат работы метода: %d\n", desc->result());
+		printf("СЂРµР»СЊР·СѓС‚Р°С‚ СЂР°Р±РѕС‚С‹ РјРµС‚РѕРґР°: %d\n", desc->result());
 		delete desc;
-		printf("Виртуальный деструктор нужен, чтобы при удалении объекта класса наследника из переменной базового класса вызывался правильный деструктор\n");
+		printf("Р’РёСЂС‚СѓР°Р»СЊРЅС‹Р№ РґРµСЃС‚СЂСѓРєС‚РѕСЂ РЅСѓР¶РµРЅ, С‡С‚РѕР±С‹ РїСЂРё СѓРґР°Р»РµРЅРёРё РѕР±СЉРµРєС‚Р° РєР»Р°СЃСЃР° РЅР°СЃР»РµРґРЅРёРєР° РёР· РїРµСЂРµРјРµРЅРЅРѕР№ Р±Р°Р·РѕРІРѕРіРѕ РєР»Р°СЃСЃР° РІС‹Р·С‹РІР°Р»СЃСЏ РїСЂР°РІРёР»СЊРЅС‹Р№ РґРµСЃС‚СЂСѓРєС‚РѕСЂ\n");
 
 	}
 	next();
 	{
-		//•	в методе1 базового класса вызывается метод2, который определен в этом же классе как невиртуальный, у класса - потомка метод2 переопределен
-		//что происходит при вызове метода1 у класса - потомка ?
+		//вЂў	РІ РјРµС‚РѕРґРµ1 Р±Р°Р·РѕРІРѕРіРѕ РєР»Р°СЃСЃР° РІС‹Р·С‹РІР°РµС‚СЃСЏ РјРµС‚РѕРґ2, РєРѕС‚РѕСЂС‹Р№ РѕРїСЂРµРґРµР»РµРЅ РІ СЌС‚РѕРј Р¶Рµ РєР»Р°СЃСЃРµ РєР°Рє РЅРµРІРёСЂС‚СѓР°Р»СЊРЅС‹Р№, Сѓ РєР»Р°СЃСЃР° - РїРѕС‚РѕРјРєР° РјРµС‚РѕРґ2 РїРµСЂРµРѕРїСЂРµРґРµР»РµРЅ
+		//С‡С‚Рѕ РїСЂРѕРёСЃС…РѕРґРёС‚ РїСЂРё РІС‹Р·РѕРІРµ РјРµС‚РѕРґР°1 Сѓ РєР»Р°СЃСЃР° - РїРѕС‚РѕРјРєР° ?
 		using namespace overriden_methods;
 		Desc desc;
-		desc.v_first_meth(); //этот метод виртуальный
-		printf("В этом случае вызывается функция second_meth базового класса Base\n");
-		desc.nv_first_meth();//этот метод не виртуальный
-		printf("В этом случае вызывается функция second_meth базового класса Base\n");
+		desc.v_first_meth(); //СЌС‚РѕС‚ РјРµС‚РѕРґ РІРёСЂС‚СѓР°Р»СЊРЅС‹Р№
+		printf("Р’ СЌС‚РѕРј СЃР»СѓС‡Р°Рµ РІС‹Р·С‹РІР°РµС‚СЃСЏ С„СѓРЅРєС†РёСЏ second_meth Р±Р°Р·РѕРІРѕРіРѕ РєР»Р°СЃСЃР° Base\n");
+		desc.nv_first_meth();//СЌС‚РѕС‚ РјРµС‚РѕРґ РЅРµ РІРёСЂС‚СѓР°Р»СЊРЅС‹Р№
+		printf("Р’ СЌС‚РѕРј СЃР»СѓС‡Р°Рµ РІС‹Р·С‹РІР°РµС‚СЃСЏ С„СѓРЅРєС†РёСЏ second_meth Р±Р°Р·РѕРІРѕРіРѕ РєР»Р°СЃСЃР° Base\n");
 	}
 	next();
 	{
@@ -181,16 +254,16 @@ int main() {
 
 		first_c* fcm[3] = { fc1,fc2,fc3 };
 		second_c* scm[3] = { sc2,sc3,tc3 };
-		printf("\nСозданы два массива указателей fcm[3] = { fc1,fc2,fc3 }\n(Массив указателей на объекты класса first_c или его потомков)\n и scm[3] = { sc2,sc3,tc3 }\n(Массив указателей на объекты класса second_c или его потомков)\n");
-		printf("\n проверим с помощью методов classname(\"name\") и isA(\"name\") к какому классу относятся объекты внутри массива:\n");
-		printf("массив fcm, метод classname()\n");
+		printf("\nРЎРѕР·РґР°РЅС‹ РґРІР° РјР°СЃСЃРёРІР° СѓРєР°Р·Р°С‚РµР»РµР№ fcm[3] = { fc1,fc2,fc3 }\n(РњР°СЃСЃРёРІ СѓРєР°Р·Р°С‚РµР»РµР№ РЅР° РѕР±СЉРµРєС‚С‹ РєР»Р°СЃСЃР° first_c РёР»Рё РµРіРѕ РїРѕС‚РѕРјРєРѕРІ)\n Рё scm[3] = { sc2,sc3,tc3 }\n(РњР°СЃСЃРёРІ СѓРєР°Р·Р°С‚РµР»РµР№ РЅР° РѕР±СЉРµРєС‚С‹ РєР»Р°СЃСЃР° second_c РёР»Рё РµРіРѕ РїРѕС‚РѕРјРєРѕРІ)\n");
+		printf("\n РїСЂРѕРІРµСЂРёРј СЃ РїРѕРјРѕС‰СЊСЋ РјРµС‚РѕРґРѕРІ classname(\"name\") Рё isA(\"name\") Рє РєР°РєРѕРјСѓ РєР»Р°СЃСЃСѓ РѕС‚РЅРѕСЃСЏС‚СЃСЏ РѕР±СЉРµРєС‚С‹ РІРЅСѓС‚СЂРё РјР°СЃСЃРёРІР°:\n");
+		printf("РјР°СЃСЃРёРІ fcm, РјРµС‚РѕРґ classname()\n");
 		for (int i = 0; i < 3; i++) {
-			printf("элемент №%d :\n", i);
+			printf("СЌР»РµРјРµРЅС‚ в„–%d :\n", i);
 			std::cout << fcm[i]->classname() << "\n";
 		}
-		printf("\nмассив fcm, метод isA\n");
+		printf("\nРјР°СЃСЃРёРІ fcm, РјРµС‚РѕРґ isA\n");
 		for (int i = 0; i < 3; i++) {
-			printf("элемент №%d: \n", i);
+			printf("СЌР»РµРјРµРЅС‚ в„–%d: \n", i);
 			if (fcm[i]->isA("first")) {
 				printf("first\n");
 			}
@@ -202,14 +275,14 @@ int main() {
 			}
 		}
 
-		printf("\nмассив scm, метод classname()\n");
+		printf("\nРјР°СЃСЃРёРІ scm, РјРµС‚РѕРґ classname()\n");
 		for (int i = 0; i < 3; i++) {
-			printf("элемент №%d :\n", i);
+			printf("СЌР»РµРјРµРЅС‚ в„–%d :\n", i);
 			std::cout << fcm[i]->classname() << "\n";
 		}
-		printf("\nмассив scm, метод isA\n");
+		printf("\nРјР°СЃСЃРёРІ scm, РјРµС‚РѕРґ isA\n");
 		for (int i = 0; i < 3; i++) {
-			printf("элемент №%d: \n", i);
+			printf("СЌР»РµРјРµРЅС‚ в„–%d: \n", i);
 			if (scm[i]->isA("first")) {
 				printf("first\n");
 			}
@@ -220,7 +293,7 @@ int main() {
 				printf("third\n");
 			}
 		}
-		printf("\nОтличие между методами classname() и isA() в том, что classname возвращает название созданного объекта,\n а метод isA() возвращает true, если объект принадлежит данному классу или является его наследником\n");
+		printf("\nРћС‚Р»РёС‡РёРµ РјРµР¶РґСѓ РјРµС‚РѕРґР°РјРё classname() Рё isA() РІ С‚РѕРј, С‡С‚Рѕ classname РІРѕР·РІСЂР°С‰Р°РµС‚ РЅР°Р·РІР°РЅРёРµ СЃРѕР·РґР°РЅРЅРѕРіРѕ РѕР±СЉРµРєС‚Р°,\n Р° РјРµС‚РѕРґ isA() РІРѕР·РІСЂР°С‰Р°РµС‚ true, РµСЃР»Рё РѕР±СЉРµРєС‚ РїСЂРёРЅР°РґР»РµР¶РёС‚ РґР°РЅРЅРѕРјСѓ РєР»Р°СЃСЃСѓ РёР»Рё СЏРІР»СЏРµС‚СЃСЏ РµРіРѕ РЅР°СЃР»РµРґРЅРёРєРѕРј\n");
 		delete fc1;
 		delete fc2;
 		delete fc3;
@@ -228,9 +301,9 @@ int main() {
 		delete sc3;
 		delete tc3;
 
-		//Приведение типов
+		//РџСЂРёРІРµРґРµРЅРёРµ С‚РёРїРѕРІ
 		next();
-		printf("Пример опасного приведения типов:\n");
+		printf("РџСЂРёРјРµСЂ РѕРїР°СЃРЅРѕРіРѕ РїСЂРёРІРµРґРµРЅРёСЏ С‚РёРїРѕРІ:\n");
 
 		first_c* fc = new first_c();
 		first_c* sc = new second_c();
@@ -242,30 +315,70 @@ int main() {
 		for (int i = 0; i < 3; i++) {
 			scm[i] = NULL;
 		}
-		printf("создадим два массива с указателями класса first fcm[3] и класса second scm[3].\nЗаполним первый массив указателями на объекты классов first,second,third\nи с помощью опасного приведения типов запишем указатели из первого массива во второй.\n");
+		printf("СЃРѕР·РґР°РґРёРј РґРІР° РјР°СЃСЃРёРІР° СЃ СѓРєР°Р·Р°С‚РµР»СЏРјРё РєР»Р°СЃСЃР° first fcm[3] Рё РєР»Р°СЃСЃР° second scm[3].\nР—Р°РїРѕР»РЅРёРј РїРµСЂРІС‹Р№ РјР°СЃСЃРёРІ СѓРєР°Р·Р°С‚РµР»СЏРјРё РЅР° РѕР±СЉРµРєС‚С‹ РєР»Р°СЃСЃРѕРІ first,second,third\nРё СЃ РїРѕРјРѕС‰СЊСЋ РѕРїР°СЃРЅРѕРіРѕ РїСЂРёРІРµРґРµРЅРёСЏ С‚РёРїРѕРІ Р·Р°РїРёС€РµРј СѓРєР°Р·Р°С‚РµР»Рё РёР· РїРµСЂРІРѕРіРѕ РјР°СЃСЃРёРІР° РІРѕ РІС‚РѕСЂРѕР№.\n");
 		for (int i = 0; i < 3; i++) {
 			if (fcm[i]->isA("second")) {
-				printf("проверка isA(\"second\") == true\nscm[%d]=fcm[%d] (fcm[%d]-> ",i,i,i); std::cout << fcm[i]->classname()<<")\n";
+				printf("РїСЂРѕРІРµСЂРєР° isA(\"second\") == true\nscm[%d]=fcm[%d] (fcm[%d]-> ",i,i,i); std::cout << fcm[i]->classname()<<")\n";
 				scm[i] = (second_c*)fcm[i];
 			}
 			else {
-				printf("проверка isA(\"second\")== false\n");
+				printf("РїСЂРѕРІРµСЂРєР° isA(\"second\")== false\n");
 				printf("fcm[% d] -> ", i);
 				std::cout<<fcm[i]->classname()<<"\n";
 			}
 		}
-		printf("\nТеперь массив scm выглядит так:\n");
+		printf("\nРўРµРїРµСЂСЊ РјР°СЃСЃРёРІ scm РІС‹РіР»СЏРґРёС‚ С‚Р°Рє:\n");
 		for (int i = 0; i < 3; i++) {
 			if (scm[i] != NULL) {
-				std::cout <<i<<"-й" << " объект: " << scm[i]->classname() << "\n";
+				std::cout <<i<<"-Р№" << " РѕР±СЉРµРєС‚: " << scm[i]->classname() << "\n";
 			}
 			else {
-				printf("%d-й объект: NULL\n", i);
+				printf("%d-Р№ РѕР±СЉРµРєС‚: NULL\n", i);
 			}
 		}
 		delete fc;
 		delete sc;
 		delete tc;
+	}
+	next();
+	{
+		int n=-1;
+		using namespace dynamic_cast_examples;
+		while (n < 2 || n>100) {
+			system("cls");
+			printf("Р’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂ С…СЂР°РЅРёР»РёС‰Р°: ");
+			std::cin >> n;
+		}
+		Point p;
+		Object** storage = new Object*[n];
+		printf("РџСЂРёРјРµРЅРёРј РјРµС‚РѕРґ set_radius Рє РѕР±СЉРµРєС‚Р°Рј Circle СЃ РїРѕРјРѕС‰СЊСЋ dynamic_cast:\n");
+		printf(",--]storage-info[--,\n");
+		for (int i = 0; i < n; i++) {
+			storage[i] = create_rand_obj();
+			storage[i]->set_id(i);
+			Circle* c_ptr = dynamic_cast<Circle*>(storage[i]);
+			Point*  p_ptr = dynamic_cast<Point*>(storage[i]);
+			Triangle* t_ptr = dynamic_cast<Triangle*>(storage[i]);
+			if (c_ptr != NULL) {
+				printf("|%d| Circle - (%d,%d,%d)", i, c_ptr->get_x(), c_ptr->get_y(), c_ptr->get_radius());
+				printf(" -- > set_radius -->");
+				c_ptr->set_radius(randomizer(4, 14));
+				printf("(%d,%d,%d)\n", c_ptr->get_x(), c_ptr->get_y(), c_ptr->get_radius());
+			}
+			else if (p_ptr != NULL) {
+				printf("|%d| Point\n", i);
+			}
+			else if (t_ptr != NULL) {
+				printf("|%d| Triangle\n", i);
+			}
+		}
+		printf("`------------------`");
+
+		printf("РџРѕРїСЂРѕР±СѓРµРј РїСЂРёРјРµРЅРёС‚СЊ РјРµС‚РѕРґ set_radius");
+		for (int i = 0; i < n; i++) {
+			delete storage[i];
+		}
+		delete[] storage;
 	}
 	return 0;
 }
